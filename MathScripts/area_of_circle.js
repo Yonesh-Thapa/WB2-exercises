0b83b2e173bf02110d1fb876c07da564c6a5c530
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-24 19:52:06 -0400

    area of circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var radius = 3;
+var pi = 3.14;
+var areaOfCircle = pi * radius**2
+
+console.log("The radius of the circle is " + radius);
+console.log("The value of pi is " + pi);
+
+console.log("The are a of circle with raidus " + radius + " is " +areaOfCircle);
