076c11eb744377cec16008705922522e54a96d19
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-24 19:35:33 -0400

    area of area_of_rectangle

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var length = 23545;
+var width = 23434;
+var area = length * width;
+
+console.log("The length of the rectangle is " + length)
+console.log("The width of the rectangle is " + width)
+console.log("The area of the rectangle is " + area)
+
