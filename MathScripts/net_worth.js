d24b437eb641e145994e29f0f81fe45a00f97658
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-24 19:46:52 -0400

    area of circle

--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -5,8 +5,8 @@ var debt = 1;
 
 var netWorth = asset - debt;
 
-console.log("Your asset is " + asset +".");
-console.log("Your debt is " + debt + ".");
-console.log("Your total net worth is " + netWorth + ".");
+console.log("Your asset is $" + asset +".");
+console.log("Your debt is $" + debt + ".");
+console.log("Your net worth is $" + netWorth + ".");
 
 
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -5,7 +5,7 @@ var tip = 20;
 
 var tipPercentage = (tip/totalAmount) * 100;
 
-console.log("The total bill is " + totalAmount);
-console.log("The total tip is "+ tip);
-console.log("The tip percentage is " + tipPercentage + "%");
+console.log("The total bill is $" + totalAmount);
+console.log("The total tip is $"+ tip);
+console.log("The tip on $" +totalAmount + " food bill is " + tipPercentage + "%");
 
