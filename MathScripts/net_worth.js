50b41a384e4251906b283811f003e5d3a8daca9b
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-24 19:13:16 -0400

    net worth math

--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -1,2 +1,12 @@
 "use strict";
 
+var asset = 1000000;
+var debt = 1;
+
+var netWorth = asset - debt;
+
+console.log("Your asset is " + asset +".");
+console.log("Your debt is " + debt + ".");
+console.log("Your total net worth is " + netWorth + ".");
+
+
