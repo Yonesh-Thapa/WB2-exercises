086cd7c4ff87693a3942f48e1523e1ab5f7f3dbc
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-24 19:42:34 -0400

    tip percentage

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var totalAmount = 100;
+var tip = 20;
+
+var tipPercentage = (tip/totalAmount) * 100;
+
+console.log("The total bill is " + totalAmount);
+console.log("The total tip is "+ tip);
+console.log("The tip percentage is " + tipPercentage + "%");
+
