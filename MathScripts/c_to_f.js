cd12037453ca7b0c452dc40bf776a44b00cc0df9
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-26 08:25:01 -0400

    celsius to farenheit

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var tempInCelsius = 80;
+var tempInFarenheit = (tempInCelsius * 1.8) + 32;
+
+
+console.log(`${tempInCelsius} degree celsius is ${tempInFarenheit.toFixed(2)}  degree farenheit.`);
--- MathScripts/f_to_c.js
+++ MathScripts/f_to_c.js
@@ -3,4 +3,4 @@
 var tempInFarenheit = 75;
 var tempInCelsius = (((tempInFarenheit -32) *5)/9);
 
-console.log(`${tempInFarenheit} degree farenheit is ${tempInCelsius.toFixed(2)} in degree celsius.`)
+console.log(`${tempInFarenheit} degree farenheit is ${tempInCelsius.toFixed(2)} degree celsius.`)
