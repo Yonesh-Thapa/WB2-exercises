41c3e6ccf65df64c6e152567ee704ab470b3e325
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-26 15:43:02 -0400

    fixed error

--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -2,5 +2,12 @@
 
 "use strict";
 
+const length = 10;
+const width = 12;
+const area = length * width;
+const numTiles = Math.ceil(area / 12);
+const numTilesWithExtra = Math.ceil(numTiles * 1.1);
+console.log(`The number of boxes of tiles required is ${numTiles} boxes.`);
+console.log(`The number of boxes with extra tiles is ${numTilesWithExtra} boxes.`);
 
 
