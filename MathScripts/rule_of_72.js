994e5bf5e9f34c1e6380954beccc303b3469d9da
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-26 08:02:01 -0400

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var interestRate = 5;
+var intitalAmount = 10000;
+var time = 72/interestRate;
+
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth " + (intitalAmount * 2) + " in " + time + " years." );
