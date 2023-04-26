0befcdd972d492762174b294debd6eca8422fbe0
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-26 08:19:18 -0400

    farenheit to degree celsius

---
+++ MathScripts/f_to_c.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var tempInFarenheit = 75;
+var tempInCelsius = (((tempInFarenheit -32) *5)/9);
+
+console.log(`${tempInFarenheit} degree farenheit is ${tempInCelsius.toFixed(2)} in degree celsius.`)
