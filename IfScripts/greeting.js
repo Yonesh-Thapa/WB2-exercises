d686572dfec6612585c35a6dc6e5c17bc7a81ab2
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-27 11:29:50 -0400

    greetings

---
+++ IfScripts/greeting.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let hour = 15;
+
+if (hour <= 10)
+{
+    console.log(`Good Morning!`)
+
+}
+else if (hour <= 17)
+{
+    console.log(`Good day!`)
+}
+else
+{
+    console.log(`Good Evening!`)
+}
