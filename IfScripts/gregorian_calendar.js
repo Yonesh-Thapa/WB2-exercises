5537f095876cfe9098cec7ba60c14c2b18067f44
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-27 11:20:30 -0400

    er

---
+++ IfScripts/gregorian_calendar.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let year = 2004;
+
+if (year % 4 == 0 && year % 100 == 0 && year % 400 ==0)
+{
+    console.log(`it is a leap year`);
+}
+else 
+{
+    console.log(`it is not a leap year`);
+}
+
+
+
+
+
+
+
+
+
+
+// } &&  (year % 100 == 0 && year % 400 == 0))
+// {
+//     console.log(`The year ${year} is a leap year`);
+
+// }
+
+// else 
+// {
+//     console.log(`The year ${year} is not a leap year`);
+// }
