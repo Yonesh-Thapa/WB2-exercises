72e9fc12a8cf09d3631461d3c52555a1c5968633
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-27 11:49:01 -0400

    leap year update

--- IfScripts/gregorian_calendar.js
+++ IfScripts/gregorian_calendar.js
@@ -1,32 +1,71 @@
-"use strict";
+let year = 1900;
 
-let year = 2004;
-
-if (year % 4 == 0 && year % 100 == 0 && year % 400 ==0)
-{
-    console.log(`it is a leap year`);
-}
+if (year % 4 == 0)
+ {
+    if (year % 100 == 0) 
+    {
+        if (year % 400 == 0) 
+        {
+            console.log(year + " is a leap year.");
+        } 
+        else 
+        {
+            console.log(year + " is not a leap year.");
+        }
+    } 
+    else 
+    {
+        console.log(year + " is a leap year.");
+    }
+} 
 else 
 {
-    console.log(`it is not a leap year`);
+    console.log(year + " is not a leap year.");
 }
 
 
 
 
 
+// "use strict";
 
+// let year = 1900;
 
-
-
-
-// } &&  (year % 100 == 0 && year % 400 == 0))
+// if (year % 4 == 0 && (year % 100 ==!0 || year % 400 ==0))
 // {
-//     console.log(`The year ${year} is a leap year`);
+//     console.log(`It is a leap year`)
 
 // }
-
 // else 
 // {
-//     console.log(`The year ${year} is not a leap year`);
+//     console.log(`It is not a leap year`)
 // }
+
+// // if (year % 4 == 0 && year % 100 == 0 && year % 400 ==0)
+// // {
+// //     console.log(`it is a leap year`);
+// // }
+// // else 
+// // {
+// //     console.log(`it is not a leap year`);
+// // }
+
+
+
+
+
+
+
+
+
+
+// // } &&  (year % 100 == 0 && year % 400 == 0))
+// // {
+// //     console.log(`The year ${year} is a leap year`);
+
+// // }
+
+// // else 
+// // {
+// //     console.log(`The year ${year} is not a leap year`);
+// // }
